5fd3766d02a6f72942d526dcf7b377990e23771b
svaronc <sebastianvaron96@gmail.com>
2024-03-04 21:25:16 -0500

    create about controller to display the page about and update the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,8 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
-
+  
+  get 'about', to: 'about#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
