fe540449bf1669c8428d260edf8be489d64d6eec
svaronc <sebastianvaron96@gmail.com>
2024-03-16 15:40:49 -0500

    test for product details page

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,8 +1,14 @@
-describe("Home Page", () => {
+describe("Product detail page", () => {
   it("should visit the home page", () => {
     cy.visit("/");
   });
   it("there is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
+  });
+  it("should click on the first product", () => {
+    cy.get(".products article:first-child a").click();
+  });
+  it("should check if the 'Add' button is present", () => {
+    cy.get(".btn").should("be.visible");
   });
 });
